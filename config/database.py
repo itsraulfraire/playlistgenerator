97472ddc4d2b976e1ebf3fb4a4426285567e688e
itsraulfraire <rfrairegaytan@gmail.com>
2025-11-05 11:45:47 -0600

    Refactor DatabaseSingleton to DatabaseConnection

--- config/database.py
+++ config/database.py
@@ -1,25 +1,20 @@
 import mysql.connector
 
-class DatabaseSingleton:
-    __instance = None
+class DatabaseConnection:
+    _instance = None
 
     @staticmethod
-    def get_instance():
-        if DatabaseSingleton.__instance is None:
-            DatabaseSingleton()
-        return DatabaseSingleton.__instance
+    def get_connection():
+        if DatabaseConnection._instance is None:
+            DatabaseConnection()
+        return DatabaseConnection._instance
 
     def __init__(self):
-        if DatabaseSingleton.__instance is not None:
-            raise Exception("Esta clase es un Singleton!")
-        else:
-            DatabaseSingleton.__instance = self
-            self.connection = mysql.connector.connect(
-                host="185.232.14.52",
-                user="u760464709_23005270_usr",
-                password="$x[QjFu>Lt9H",
-                database="u760464709_23005270_bd"
-            )
-
-    def get_connection(self):
-        return self.connection
+        if DatabaseConnection._instance is not None:
+            return
+        DatabaseConnection._instance = mysql.connector.connect(
+            host="185.232.14.52",
+            user="u760464709_23005270_usr",
+            password="$x[QjFu>Lt9H",
+            database="u760464709_23005270_bd"
+        )
